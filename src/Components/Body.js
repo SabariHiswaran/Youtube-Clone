27113258cd770acf6590904a4ebd21de6bd00618
Sabari <sabarihiswaran555@gmail.com>
2023-03-07 11:47:28 +0530

    Plan-completed

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideMenuBar from './SideMenuBar'
+import Videos from './Videos'
+
+const Body = () => {
+  return (
+   <>
+<SideMenuBar/>
+<Videos/>
+   </>
+  )
+}
+
+export default Body
