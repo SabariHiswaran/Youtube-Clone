30ec45fe25f542c82e854b3ea307820f743d92b9
Sabari <sabarihiswaran555@gmail.com>
2023-03-12 20:02:41 +0530

    updated menu color of first item

--- src/Components/CommentList.js
+++ src/Components/CommentList.js
@@ -5,10 +5,10 @@ const CommentList = ( {commentsData}) => {
    
   return( 
     commentsData?.map ((item,index) => {
-        console.log(item)
+       
             return (
-                <div>
-                <Comment  commentData = {item} key={index}/>
+                <div key={index}>
+                <Comment  commentData = {item} />
                 <div className='pl-5 border border-l-black'>
                  {item.reply && <CommentList commentsData={item.reply}/>}
                 </div>
--- src/Components/VideoMenu.js
+++ src/Components/VideoMenu.js
@@ -3,19 +3,18 @@ const VideoMenu = () => {
 
     const menuitems = ["All", "Mixes", "Music", "Chess", "BodyBuilding", "Computer Programing", "Sales", " News", "Thoughts","Recently Uploaded" , "New to You","Watched"]
 
-    
-
     return (
 
         <div className="flex ">
 
                 {menuitems.map((item,index) => {
 
-                    // const colorOfMenu = item === menuitems[0] ? "bg-black-200 text-white-500" : "bg-gray-200 text-black-500"
-
+                     const colorOfMenu = item === menuitems[0] ? "font-bold text-white bg-black" : "bg-gray-200 text-black-500"
+                    console.log(item === menuitems[0])
+                   
                     return (
 
-                        <button key={index} className="bg-gray-200 text-black-500 pr-3 pl-3 ml-3 border rounded-md"> {item} </button>
+                        <button key={index} className= {`${colorOfMenu} pr-3 pl-3 ml-3 border rounded-md`}> {item} </button>
 
                     )
                 })}
