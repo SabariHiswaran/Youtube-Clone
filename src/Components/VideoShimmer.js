01fac6595aa68b86b10971f52ce4bc8af7acdd0d
Sabari <sabarihiswaran555@gmail.com>
2023-03-15 17:02:51 +0530

    shimmer completed

--- src/Components/VideoShimmer.js
+++ src/Components/VideoShimmer.js
@@ -2,28 +2,27 @@ import React from 'react'
 
 const VideoShimmer = () => {
   return (
-    <>
+    <div className='flex flex-wrap ml-10'>
     {Array(50).fill("").map((items,index) => {
         return (
-            <div className='flex flex-wrap ml-10'>
-            <div className='p-2 ml-8 mt-10 w-[360px] bg-gray-100 rounded-xl  h-[190px] hover:cursor-pointer'>
+            <div className='p-2 ml-8 mt-10 w-[360px] '>
+            <div className="rounded-xl bg-gray-200  w-[360px] h-[200px]" >
             </div>
-        
             <div className='flex items-start justify-between mt-3 w-[360px]'>
             <div className='w-[40px] '>
-              <img src="" className="rounded-[50%] h-10 bg-gray-100" alt="" />
+              <img src="" className="rounded-[50%] h-10 bg-gray-200" alt="" />
             </div>
             <div className='w-[320px] ml-3'>
-              <h3 className='bg-gray-100'> </h3>
-              <p  className='bg-gray-100'> </p>
-          
+              <h3 className='font-bold text-sm bg-gray-200 w-[250px] h-[20px]'> </h3>
+              <p className='bg-gray-200 mt-3 w-[200px] h-[20px]'> </p>
             </div>
           </div>
           </div>
+         
         )
     }
 )}
-    </>
+     </div>
   )
 }
 
