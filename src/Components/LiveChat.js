8d72bfe55b05b362dff2aa1be0a1c21c89cb4c1b
Sabari <sabarihiswaran555@gmail.com>
2023-03-18 21:47:51 +0530

    live chat successfully completed

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { livechat } from '../Utils/chatSlice'
 import ChatMessage from './ChatMessage'
 import { generate, makeid } from './helper'
 
-import {RiArrowDropDownLine} from 'react-icons/ri'
-import {CiMenuKebab} from 'react-icons/ci'
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { CiMenuKebab } from 'react-icons/ci'
+import { AiOutlineSend } from 'react-icons/ai'
 
 const LiveChat = () => {
 
+    const [liveChatMessage , setLiveChatMessage] = useState("")
 
     const selector = useSelector((store) => store.livechat.chatMessages)
 
@@ -18,10 +20,10 @@ const LiveChat = () => {
 
         //API Polling
 
-      const messageInterval =   setInterval(handleMessage, 2000)
+        const messageInterval = setInterval(handleMessage, 2000)
 
         return () => clearInterval(messageInterval)
-    },[])
+    }, [])
 
     const handleMessage = () => {
         dispatch(livechat(
@@ -32,22 +34,58 @@ const LiveChat = () => {
         ))
     }
 
+    const handleLiveChatMessage = (e) => {
+            setLiveChatMessage(e.target.value)
+           
+    }
+
+    const handleSubmit = () => {
+        dispatch(livechat(
+            {
+                profilename: generate(),
+                comment: liveChatMessage
+            }
+        ))
+        setLiveChatMessage("")
+    }
+
     return (
         <>
             <div className='h-[50px] w-[410px] ml-8 p-3 bg-white border border-gray-400 border-b-0 rounded-t-md'>
-            <h1 className='text-bold flex items-center'> 
-                <span> Top Chat </span>
-               <span className='text-xl mr-auto'> <RiArrowDropDownLine/></span>
-               <span > <CiMenuKebab/> </span>
-                </h1> 
-                </div>
-            <div className='w-[410px] h-[428px] ml-8 p-3 border border-gray-400 rounded-b-md bg-gray-100 overflow-y-scroll flex flex-col-reverse'>
-                {selector?.map((item,index) => {
+                <h1 className='text-bold flex items-center'>
+                    <span> Top Chat </span>
+                    <span className='text-xl mr-auto'> <RiArrowDropDownLine /></span>
+                    <span > <CiMenuKebab /> </span>
+                </h1>
+            </div>
+
+            <div className='w-[410px] h-[428px] ml-8 p-3 border border-gray-400  bg-gray-100 overflow-y-scroll flex flex-col-reverse'>
+                {selector?.map((item, index) => {
                     return (
-                        <ChatMessage message={ item } key={index}/>
+                        <ChatMessage message={item} key={index} />
                     )
                 })}
-               
+
+            </div>
+
+            <div className='h-[50px] w-[410px] ml-8 p-3 bg-white border border-gray-400 border-t-0 rounded-b-md'>
+
+                    <div className='flex items-center'>
+                        <span className='pl-3'>
+                            <input 
+                            type="text" 
+                            placeholder='Say Something' 
+                            className=' border border-t-0 border-l-0 border-r-0 border-b-gray-400 w-[330px] placeholder:text-xs focus:outline-none' 
+                            value={liveChatMessage}
+                            onChange={(e) => handleLiveChatMessage(e)}
+                            />
+                        </span>
+                        <span className='pl-3'>
+                            <AiOutlineSend onClick={handleSubmit} className="hover:cursor-pointer"/>
+                        </span>
+                    </div>
+                
+
             </div>
         </>
     )
