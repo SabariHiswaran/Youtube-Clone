5684395d3fba9c2f479977f0c35e2694ea2a0d2c
Sabari <sabarihiswaran555@gmail.com>
2023-03-22 13:48:29 +0530

    relatedvideo shimmer completed successfully

--- src/Components/RelatedVideoShimmer.js
+++ src/Components/RelatedVideoShimmer.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const RelatedVideoShimmer = () => {
   return (
-    <div>RelatedVideoShimmer</div>
+    <div>
+   {
+    Array(15).fill("").map((item , index) => {
+      return (
+        <div className='flex w-[410px] h-[90px] mt-8'   key={index}>
+
+        <div className='w-[160px] h-[90px]'>
+            <div className="rounded-md h-[90px] w-[160px] bg-gray-300">
+            </div>
+        </div>
+
+        <div className='w-[250px] pl-2 h-[90px]'>
+
+                <h1 className='bg-gray-300 w-[250px] h-[20px]  mt-2' > </h1>
+                <p className='bg-gray-300 w-[150px] h-[20px] mt-2'>  </p>
+
+        </div>
+
+
+    </div>
+      )
+    })
+   }
+   </div>
   )
 }
 
