d79d0ca325e5fba3c957e907aadd87f0f61c8559
Sabari <sabarihiswaran555@gmail.com>
2023-03-25 23:59:40 +0530

    comments section replies styled

--- src/Components/CommentList.js
+++ src/Components/CommentList.js
@@ -9,11 +9,18 @@ const CommentList = ( {commentsData}) => {
             return (
                 <div key={item.id} className="mt-4">
                 <Comment  commentData = {item} />
-                <div>
-                <div className='pl-5 ml-14  w-[935px] border-0 border-l-2 border-l-red-600'>
+                {item.replies.length ===0 ?
+                  ""
+                  :
+                  <div className='ml-14 w-[935px] h-[30px] border-0 border-l-2 border-l-gray-600'>
+                    Replies
+                  </div>
+                  
+                }
+                <div className='ml-7 w-[935px] '>
                  {item.replies && <CommentList commentsData={item.replies}/>}
                 </div>
-                </div>
+               
                 </div>
             )
         })
