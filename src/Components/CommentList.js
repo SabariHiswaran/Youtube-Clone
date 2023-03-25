c5a324be604e361cd32e200c586c8c996edd523d
Sabari <sabarihiswaran555@gmail.com>
2023-03-25 23:41:01 +0530

    comments data id as key

--- src/Components/CommentList.js
+++ src/Components/CommentList.js
@@ -4,10 +4,10 @@ import Comment from './Comment'
 const CommentList = ( {commentsData}) => {
    
   return( 
-    commentsData?.map ((item,index) => {
+    commentsData?.map ((item) => {
        
             return (
-                <div key={index} className="mt-4">
+                <div key={item.id} className="mt-4">
                 <Comment  commentData = {item} />
                 <div className='pl-5  w-[935px]'>
                  {item.reply && <CommentList commentsData={item.reply}/>}
