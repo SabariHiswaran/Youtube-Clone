8796ee2f08dc9879a32a7e4ebcd5c97449aed665
Sabari <sabarihiswaran555@gmail.com>
2023-03-29 11:02:56 +0530

    added new api key

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,6 +2,8 @@
 
 export const GOOGLE_API_KEY = "AIzaSyBtXLNO3vwaomPrOQgyxYZegRAOoHhY3Vg"
 
+// export const GOOGLE_API_KEY = "AIzaSyDeNXW4PLogKTOO4kKV7CENgT9ooRLJRDs"
+
 export const FETCH_VIDEO_API =
  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`
 
